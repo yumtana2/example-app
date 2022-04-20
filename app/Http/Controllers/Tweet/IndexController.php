4c5ad46f5a10f2fa53a3cc7d5c59c449eef11cbc
yumiko tanaka <tnk.ymk@gmail.com>
2022-04-20 14:00:11 +0900

    ダミーデータ作成、一覧表示機能作成

--- app/Http/Controllers/Tweet/IndexController.php
+++ app/Http/Controllers/Tweet/IndexController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Tweet;
 
 use App\Http\Controllers\Controller;
+use App\Models\Tweet;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
@@ -10,11 +11,12 @@ class IndexController extends Controller
     /**
      * Handle the incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function __invoke(Request $request)
     {
-        return 'Single Action!';
+        $tweets = Tweet::all();
+        return view('tweet.index')->with('tweets', $tweets);
     }
 }
