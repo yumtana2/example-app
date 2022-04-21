a9ebac7117766a60e0197dad0254280ff492d879
yumiko tanaka <tnk.ymk@gmail.com>
2022-04-21 21:39:17 +0900

    ログイン機能を追加 #6

--- routes/web.php
+++ routes/web.php
@@ -16,11 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+// sample
 Route::get('/sample', [\App\Http\Controllers\Sample\IndexController::class, 'show']);
-
 Route::get('/sample/{id}', [\App\Http\Controllers\Sample\IndexController::class, 'showId']);
 
+// tweet
 Route::get('/tweet', \App\Http\Controllers\Tweet\IndexController::class)
     ->name('tweet.index');
 
@@ -35,3 +35,9 @@ Route::put('/tweet/update/{tweetId}', \App\Http\Controllers\Tweet\Update\PutCont
 
 Route::delete('/tweet/delete/{tweetId}', \App\Http\Controllers\Tweet\DeleteController::class)
     ->name('tweet.delete');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
