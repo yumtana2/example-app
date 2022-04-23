70243f6b568cc815ca62b97c71d80528fc24d810
yumiko tanaka <tnk.ymk@gmail.com>
2022-04-23 23:35:21 +0900

    フロントエンド作成(削除コンポーネント化) #12

--- app/Http/Controllers/Tweet/CreateController.php
+++ app/Http/Controllers/Tweet/CreateController.php
@@ -3,9 +3,8 @@
 namespace App\Http\Controllers\Tweet;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Tweet\createRequest;
+use App\Http\Requests\Tweet\CreateRequest;
 use App\Models\Tweet;
-use Illuminate\Http\Request;
 
 class CreateController extends Controller
 {
@@ -15,7 +14,7 @@ class CreateController extends Controller
      * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\RedirectResponse
      */
-    public function __invoke(createRequest $request)
+    public function __invoke(CreateRequest $request)
     {
         $tweet = new Tweet;
         $tweet->user_id = $request->userId();
--- app/Http/Requests/Tweet/CreateRequest.php
+++ app/Http/Requests/Tweet/CreateRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests\Tweet;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class createRequest extends FormRequest
+class CreateRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
