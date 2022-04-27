b9a7f74743b29929d2b4287181543463db1e3cd9
yumtana2 <tnk.ymk@gmail.com>
2022-04-27 11:14:09 +0900

    フィーチャーテストを追加

---
+++ tests/Feature/Tweet/DeleteTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature\Tweet;
+
+use App\Models\Tweet;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class DeleteTest extends TestCase
+{
+    // テスト実行前後、DBが初期化される
+    use RefreshDatabase;
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_delete_successed()
+    {
+        $user = User::factory()->create(); // ユーザーを作成
+        $tweet = Tweet::factory()->create(['user_id' => $user->id]); // つぶやきを作成
+
+        $this->actingAs($user); // 指定したユーザーでログインした状態にする
+
+        $response = $this->delete('/tweet/delete/' . $tweet->id);
+        $response->assertRedirect('/tweet');
+    }
+}
