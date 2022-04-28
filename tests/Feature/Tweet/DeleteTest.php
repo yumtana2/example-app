20bef7f28f8f4817b9d3af201270c5e4f80dd468
yumtana2 <tnk.ymk@gmail.com>
2022-04-28 14:13:26 +0900

    不要なインポート分を削除

--- tests/Feature/Tweet/DeleteTest.php
+++ tests/Feature/Tweet/DeleteTest.php
@@ -5,7 +5,6 @@ namespace Tests\Feature\Tweet;
 use App\Models\Tweet;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class DeleteTest extends TestCase
